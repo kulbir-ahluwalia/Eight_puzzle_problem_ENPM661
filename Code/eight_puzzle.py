200da94ab280017df9cc457964445b4e73c335a8
kulbir-ahluwalia <kulbir@terpmail.umd.edu>
2020-02-23 04:43:38 -0500

    added solvability check function

--- Code/eight_puzzle.py
+++ Code/eight_puzzle.py
@@ -1,7 +1,10 @@
 import numpy as np
 
-# Code to take initial state of the eight puzzle problem
+####################################################################################################
+# Functions to take the initial state of the eight puzzle, check for repitition and solvability
+####################################################################################################
 
+# Code to take initial state of the eight puzzle problem
 def initial_state():
     print("Enter the initial state of the eight puzzle problem row wise")
     start_node = [0,0,0, 0,0,0, 0,0,0]
@@ -12,13 +15,11 @@ def initial_state():
             exit(0)
           
         else:
-            start_node[i] = value
-            
+            start_node[i] = value            
     return start_node
 
 # Code to check for repetition of numbers
-def repetition_check(node_for_check):
-  
+def repetition_check(node_for_check):  
     for x in range(9):  
         no_of_repetition = 0      
         entry = node_for_check[x]
@@ -30,14 +31,27 @@ def repetition_check(node_for_check):
             print("An entry for the eight puzzle has been repeated, Please restart program")
             exit(0)
 
-start_node = initial_state()
-print("The initial state in row wise representation is  : ", start_node)    
+# test code to check if the eight puzzle is solvable or not
+def check_solvability(node_check):
+    inversions = 0
+    for x in range(len(node_check)):
+        for y in range(len(node_check[(x+1):])):
+            # Uncomment following line to see the values being compared
+            #print(node_check[x],"  ", node_check[x+y+1])
+            # to ignore the blank tile
+            if node_check[x+y+1]>0:
+                if node_check[x] > node_check[x+y+1]:
+                    inversions = inversions + 1
+    #print(inversions) #to check the number of inversions
+    
+    if inversions % 2 == 0:
+        return True
+    else:
+        return False
 
-repetition_check(start_node)
-
-
-goal_node = [1,2,3,4,5,6,7,8,0]
-print("The goal state in row wise representation is  : ", goal_node) 
+####################################################################################################
+# Functions to move the zero tile left, right, up and down
+####################################################################################################
 
 # function to move the blank (zero) tile left
 def ActionMoveLeft(node_zero_unmoved):
@@ -52,8 +66,6 @@ def ActionMoveLeft(node_zero_unmoved):
         possibility = True
         return possibility, node_zero_unmoved, node_zero_moved
 
-
-
 # function to move the blank (zero) tile right
 def ActionMoveRight(node_zero_unmoved):
     zero_position = BlankTileLocation(node_zero_unmoved)
@@ -66,7 +78,6 @@ def ActionMoveRight(node_zero_unmoved):
         node_zero_moved[zero_position+1] = 0
         possibility = True
         return possibility, node_zero_unmoved, node_zero_moved
-
 
 # function to move the blank (zero) tile up
 def ActionMoveUp(node_zero_unmoved):
@@ -81,7 +92,6 @@ def ActionMoveUp(node_zero_unmoved):
         possibility = True
         return possibility, node_zero_unmoved, node_zero_moved
 
-
 # function to move the blank (zero) tile down
 def ActionMoveDown(node_zero_unmoved):
     zero_position = BlankTileLocation(node_zero_unmoved)
@@ -94,6 +104,37 @@ def ActionMoveDown(node_zero_unmoved):
         node_zero_moved[zero_position+3] = 0
         possibility = True
         return possibility, node_zero_unmoved, node_zero_moved
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+start_node = initial_state()
+print("The initial state in row wise representation is  : ", start_node)    
+
+repetition_check(start_node)
+
+
+goal_node = [1,2,3,4,5,6,7,8,0]
+print("The goal state in row wise representation is  : ", goal_node) 
 
 
 
