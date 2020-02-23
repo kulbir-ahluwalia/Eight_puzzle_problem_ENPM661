841a04bf622fbefaee12f40fb336aa88061dab8c
kulbir-ahluwalia <kulbir@terpmail.umd.edu>
2020-02-22 20:55:43 -0500

    changes to code

--- Code/eight_puzzle.py
+++ Code/eight_puzzle.py
@@ -1,5 +1,4 @@
 # Code to take initial state of the eight puzzle problem
-
 def initial_state():
     print("Enter the initial state of the eight puzzle problem element wise")
     start_node = [0,0,0, 0,0,0, 0,0,0]
@@ -15,4 +14,21 @@ def initial_state():
     print("The initial state in row wise representation is : ", start_node) 
 
 
-initial_state()
+# Code to check for repetition of numbers
+def repetition_check(node_for_check):
+    for x in range(9):
+        no_of_repetition = 0
+        entry = node_for_check[x]
+        for y in range(9):
+            if entry == node_for_check[y]:
+                no_of_repetition = no_of_repetition +1
+             
+    if no_of_repetition >= 1:
+        print("An entry for the eight puzzle has been repeated, Please restart program")
+        exit(0)
+
+start_node = initial_state()
+print("The initial state in row wise representation is  : ", start_node)    
+
+repetition_check(start_node)
+
