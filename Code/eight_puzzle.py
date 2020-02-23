8add040d6f1a28fb6d60bc8cc39079acf4a27129
kulbir-ahluwalia <kulbir@terpmail.umd.edu>
2020-02-22 20:19:00 -0500

    changes to jupyter notebook

--- Code/eight_puzzle.py
+++ Code/eight_puzzle.py
@@ -1,1 +1,18 @@
-class puzzle
+# Code to take initial state of the eight puzzle problem
+
+def initial_state():
+    print("Enter the initial state of the eight puzzle problem element wise")
+    start_node = [0,0,0, 0,0,0, 0,0,0]
+    for i in range(9):
+        value = int(input("Enter the " + str(i+1) + "th value of the eight puzzle\n"))
+        if value >8 or value<0:
+            print("Enter a value from 0 to 8 only, Run program again please")
+            exit(0)
+          
+        else:
+            start_node[i] = value
+            
+    print("The initial state in row wise representation is : ", start_node) 
+
+
+initial_state()
